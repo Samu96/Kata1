dc847aa53c7e56c4d5bec8d3c80c685b52a51ce2
usuario <usuario@10.22.148.56>
2017-09-22 08:13:31 +0100

    Revisar date

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class Kata1{
     public static void main(String[] args){
-        Person person = new Person("Samu", new Date(96,04,15));
+        Person person = new Person("Samu", new Date(96,9,8));
         System.out.println(person.getName()+ " tiene  " + person.getAge());
     }
     
